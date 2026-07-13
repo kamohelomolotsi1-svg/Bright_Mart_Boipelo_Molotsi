6ff7b16b8cd70e70c41195b7eb227d90c270df88
kamohelomolotsi1-svg <kamohelomolotsi1@gmail.com>
2026-07-13 18:55:44 +0200

    Creating_stg_dim_store

---
+++ 0.4. create_stg_dim/0.4.4. create_stg_dim_store.sql
@@ -0,0 +1,44 @@
+SELECT [store_name],
+       [store_city],
+       [store_province],
+       [store_region],
+       [store_manager]
+FROM [stg_brightlearn_express].[dbo].[BrightLearn_Raw_Data]
+
+----------------------------------------------------------------------------------
+
+USE stg_brightlearn_express;
+GO
+
+
+IF OBJECT_ID(N'[stg_brightlearn_express].[dbo].[stg_dim_store]', N'U') IS NULL
+CREATE TABLE [stg_brightlearn_express].[dbo].[stg_dim_store] (
+       [store_name] VARCHAR(255),
+       [store_city] VARCHAR(255),
+       [store_province] VARCHAR(255),
+       [store_region] VARCHAR(255),
+       [store_manager] VARCHAR(255)
+
+       );
+
+------------------------------------------------------------------------------------------
+
+INSERT INTO [stg_brightlearn_express].[dbo].[stg_dim_store] (
+       [store_name],
+       [store_city],
+       [store_province],
+       [store_region],
+       [store_manager]
+
+)
+
+SELECT DISTINCT    [store_name],
+                   [store_city],
+                   [store_province],
+                   [store_region],
+                   [store_manager]
+FROM [stg_brightlearn_express].[dbo].[BrightLearn_Raw_Data]
+
+---------------------------------------------------------------------------------
+
+    SELECT * FROM [stg_brightlearn_express].[dbo].[stg_dim_store]
